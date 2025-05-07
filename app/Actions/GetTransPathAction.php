7b113fb9e1055f5ca635e5d9ff94cd8bcac2b2ef
marco76tv <marco.sottana@gmail.com>
2025-05-07 12:26:12 +0200

    .

--- app/Actions/GetTransPathAction.php
+++ app/Actions/GetTransPathAction.php
@@ -5,16 +5,10 @@ declare(strict_types=1);
 namespace Modules\Lang\Actions;
 
 use Illuminate\Support\Str;
-<<<<<<< HEAD
 use Webmozart\Assert\Assert;
 use Nwidart\Modules\Facades\Module;
 use Spatie\QueueableAction\QueueableAction;
 use Modules\Xot\Actions\Module\GetModulePathByGeneratorAction;
-=======
-use Nwidart\Modules\Facades\Module;
-use Spatie\QueueableAction\QueueableAction;
-use Webmozart\Assert\Assert;
->>>>>>> 592ff03 (.)
 
 class GetTransPathAction
 {
@@ -40,18 +34,8 @@ class GetTransPathAction
             $module_path = Str::of($module_path)->beforeLast('/')->toString();
         }
         $lang = app()->getLocale();
-<<<<<<< HEAD
-        //$relativePath = config('modules.paths.generator.lang.path');
-
-        // Utilizziamo module_path e verifichiamo che restituisca una stringa
-        //$lang_path = module_path($ns, $relativePath);
+        // Utilizziamo l'action centralizzata per recuperare il percorso con validazione integrata
         $lang_path = app(GetModulePathByGeneratorAction::class)->execute($ns, 'lang');
-=======
-        $relativePath = config('modules.paths.generator.lang.path');
-
-        // Utilizziamo module_path e verifichiamo che restituisca una stringa
-        $lang_path = module_path($ns, $relativePath);
->>>>>>> 592ff03 (.)
         Assert::string($lang_path, 'Il percorso del modulo deve essere una stringa');
 
         $file_name = $piece[0] ?? '';
