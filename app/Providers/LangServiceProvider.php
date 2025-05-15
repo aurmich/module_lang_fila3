1aa9167ffdac10570e3e8f57659bdced6f30d7bf
Malebestia <malebestia@yahoo.it>
2025-05-15 10:05:47 +0200

    .

--- app/Providers/LangServiceProvider.php
+++ app/Providers/LangServiceProvider.php
@@ -17,7 +17,10 @@ use Filament\Tables\Filters\BaseFilter;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Container\Container;
 use Illuminate\Support\Facades\View;
+<<<<<<< HEAD
 use Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider;
+=======
+>>>>>>> bde0734 (.)
 use Modules\Lang\Actions\Filament\AutoLabelAction;
 use Modules\Lang\Services\TranslatorService;
 use Modules\Xot\Providers\XotBaseServiceProvider;
@@ -47,6 +50,7 @@ class LangServiceProvider extends XotBaseServiceProvider
     public function register(): void
     {
         parent::register();
+<<<<<<< HEAD
 
         // Registra il service provider di laravel-localization
         $this->app->register(LaravelLocalizationServiceProvider::class);
@@ -56,6 +60,8 @@ class LangServiceProvider extends XotBaseServiceProvider
         //    __DIR__.'/../config/laravel-localization.php', 'laravel-localization'
         //);
 
+=======
+>>>>>>> bde0734 (.)
         // --dalla doc in register ... ma non funziona, funziona in boot
         // $this->registerTranslator();
     }
@@ -74,7 +80,10 @@ class LangServiceProvider extends XotBaseServiceProvider
 
     public function registerFilamentLabel(): void
     {
+<<<<<<< HEAD
 
+=======
+>>>>>>> bde0734 (.)
         Field::configureUsing(function (Field $component) {
             $component = app(AutoLabelAction::class)->execute($component);
             Assert::isInstanceOf($component, Field::class);
@@ -89,9 +98,12 @@ class LangServiceProvider extends XotBaseServiceProvider
                 }
                 $component->validationMessages($typedMessages);
             }
+<<<<<<< HEAD
             $component = app(AutoLabelAction::class)->execute($component,'placeholder');
             $component = app(AutoLabelAction::class)->execute($component,'helperText');
             $component = app(AutoLabelAction::class)->execute($component,'description');
+=======
+>>>>>>> bde0734 (.)
 
             return $component;
         });
--- docs/README.md
+++ docs/README.md
@@ -445,6 +445,10 @@ class UserController extends Controller
 
 ---
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> bde0734 (.)
 ## Notification System
 - Email Templates
 - SMS Templates
@@ -778,6 +782,7 @@ class UserController extends Controller
 - Bug traduzioni
 - Problemi cache
 - Errori namespace
+<<<<<<< HEAD
 
 ## Politica, Filosofia, Religione, Etica, Zen
 
@@ -786,3 +791,5 @@ class UserController extends Controller
 - **Religione**: Laicità, rispetto di tutte le fedi, libertà di pensiero.
 - **Etica**: Onestà, rispetto, responsabilità, attenzione all'impatto sociale e ambientale.
 - **Zen**: Semplicità, concentrazione sul presente, armonia e serenità nello sviluppo.
+=======
+>>>>>>> bde0734 (.)
